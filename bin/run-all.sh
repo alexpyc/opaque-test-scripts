3bae19e024713a5ec6326419dc4a64b612515de3
Alexander Pui Yiu Chan <alexpychan.pyac@gmail.com>
2017-11-21 14:46:48 +0800

    Fix mistake 2

--- bin/run-all.sh
+++ bin/run-all.sh
@@ -4,32 +4,32 @@ num_partition=$1
 
 for i in `seq 3 6`
 do
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecureQ1 $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecureQ2 $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecureQ3 $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedQ1 $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedQ2 $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedQ3 $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousQ1 $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousQ2 $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousQ3 $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecureLSR $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedLSR $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousLSR $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecureQ1 ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecureQ2 ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecureQ3 ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedQ1 ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedQ2 ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedQ3 ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousQ1 ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousQ2 ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousQ3 ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecureLSR ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedLSR ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousLSR ./target/tests-1.jar $num_partition $i >> ./console.log
 done
 
 for i in `seq 8 20`
 do
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecurePR $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedPR $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousPR $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecurePR ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedPR ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousPR ./target/tests-1.jar $num_partition $i >> ./console.log
 done
 
 for i in `seq 0 13`
 do
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecureJoin $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedJoin $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousJoin $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.JoinGene $num_partition $i >> ./console.log
-    $SPARK_HOME/bin/spark-submit --class com.alexpyc.JoinTreatment $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.InsecureJoin ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.EncryptedJoin ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.ObliviousJoin ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.JoinGene ./target/tests-1.jar $num_partition $i >> ./console.log
+    $SPARK_HOME/bin/spark-submit --class com.alexpyc.JoinTreatment ./target/tests-1.jar $num_partition $i >> ./console.log
 done
